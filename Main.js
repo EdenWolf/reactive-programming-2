a531a4da168794588819791d0c07308770b42510
EdenWolf <edenwolfson93@gmail.com>
2022-01-06 20:24:31 +0200

    reading all the input files

--- Main.js
+++ Main.js
@@ -8,21 +8,38 @@ const inputFilesPaths = [
 
 var fs = require("fs");
 
-client.startClient(
-  "id",
-  "portNumber",
-  "str",
-  "clientsList",
-  "stringOperations"
-);
-
 function readInputFile(inputPath) {
   const data = fs.readFileSync(inputPath, "utf-8", function (err, data) {
     return data;
   });
-  console.log(data);
+
+  return data;
+}
+
+function getList(dataArray) {
+  const list = [];
+
+  let str;
+  while (str !== "") {
+    str = dataArray.shift();
+    if (str !== "") {
+      list.push(str);
+    }
+  }
+  return list;
 }
 
 for (let i = 0; i < inputFilesPaths.length; i++) {
-  readInputFile(inputFilesPaths[i]);
+  const data = readInputFile(inputFilesPaths[i]);
+  const dataArray = data.replace(/\r/g, "").split("\n");
+
+  const clientId = dataArray.shift();
+  const portNumber = dataArray.shift();
+  const str = dataArray.shift();
+  dataArray.shift();
+
+  const clientsList = getList(dataArray);
+  const stringOperations = getList(dataArray);
+
+  client.startClient(clientId, portNumber, str, clientsList, stringOperations);
 }
