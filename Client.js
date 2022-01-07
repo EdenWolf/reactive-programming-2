0eda5965baf5f4444a17f49d63a0be730c215db7
EdenWolf <edenwolfson93@gmail.com>
2022-01-07 20:34:31 +0200

    changed client to be an object

--- Client.js
+++ Client.js
@@ -1,56 +1,129 @@
-const axios = require("axios");
-
-const http = require("http");
 const WebSocket = require("ws");
 
-function client(id, portNumber, str, clientsList, stringOperations) {
-  // create array with all the information about the othere clients
-  console.log("\nI am client: " + id);
-  const clientsArray = [];
-
-  for (let i = 0; i < clientsList.length; i++) {
-    const clientData = clientsList[i].split(" ");
-    clientsArray.push({
-      id: clientData[0],
-      host: clientData[1],
-      port: clientData[2],
-    });
-  }
-
-  const biggerClients = clientsArray.filter((client) => id < client.id);
-
-  // Connect to clients with bigger id
-  for (let i = 0; i < biggerClients.length; i++) {
-    console.log("try to connect: " + biggerClients[i].port);
-    const socket = new WebSocket(`ws://localhost:${biggerClients[i].port}`);
-
-    socket.onopen = function () {
-      socket.send("hi!");
-    };
-
-    socket.onmessage = function ({ data }) {
-      console.log("the message is: ");
+const client = {
+  // Initial time stemp
+  id: -1,
+  str: "",
+  TS: [0, -1],
+  server: undefined,
+  portNumber: -1,
+  clientsMap: new Map(),
+  messagesTypes: {
+    connection: "connection",
+    goodbye: "googbye",
+    update: "update",
+  },
+  operationTypes: {
+    insert: "insert",
+    delete: "delete",
+  },
+  messageTypeHandler: {
+    connection: (data) => {
       console.log(data);
+    },
+    goodbye: (data) => {
+      console.log(data);
+    },
+    // insert: (data) => {
+    //   console.log(data);
+    //   clientsMap.forEach((value) => {
+    //     if (value.socket != undefined) {
+    //       value.socket.send(
+    //         JSON.stringify({ id: id, type: this.operationTypes.update })
+    //       );
+    //     }
+    //   });
+    // },
+    update: (data) => {
+      console.log(data);
+    },
+  },
+  handleMessage: function (data) {
+    this.messageTypeHandler[data.type](data);
+    console.log(this.portNumber);
+
+    // TS[0] = Math.max(TS[0], data.ts[0]);
+    // TS[0]++;
+  },
+  // Create the server
+  createServer: async function (id, portNum) {
+    this.id = id;
+    this.portNumber = portNum;
+    this.TS[1] = id;
+    console.log("port: " + this.portNumber);
+    this.server = new WebSocket.Server({ port: this.portNumber });
+    console.log("server created");
+    this.server.on("connection", (serverSocket) => {
+      serverSocket.on("message", (message) => {
+        this.handleMessage(JSON.parse(message));
+      });
+    });
+  },
+  connectToOtherClients: async function (id, clientsList) {
+    // Create array with all the information about the othere clients
+    const biggerClients = new Map();
+
+    for (let i = 0; i < clientsList.length; i++) {
+      const clientData = clientsList[i].split(" ");
+      const newClient = {
+        host: clientData[1],
+        port: clientData[2],
+        socket: undefined,
+      };
+      this.clientsMap.set(clientData[0], newClient);
+      if (clientData[0] > id) {
+        biggerClients.set(clientData[0], newClient);
+      }
+    }
+
+    // Connect to clients with bigger id
+    biggerClients.forEach((value) => {
+      const socket = new WebSocket(`ws://localhost:${value.port}`);
+
+      socket.onopen = () => {
+        socket.send(
+          JSON.stringify({ id: id, type: this.messagesTypes.connection })
+        );
+        value.socket = socket;
+      };
+
+      // Handle recieved data
+      socket.onmessage = ({ data }) => {
+        const msgData = JSON.parse(data);
+        handleMessage(msgData);
+
+        TS = Math.max(TS, msgData.ts);
+        TS++;
+      };
+    });
+  },
+  sleep: (delay) => new Promise((resolve) => setTimeout(resolve, delay)),
+  startClient: async function (str, stringOperations) {},
+};
+
+async function startClient(id, portNumber, str, clientsList, stringOperations) {
+  const sleep = (delay) => new Promise((resolve) => setTimeout(resolve, delay));
+
+  function doTask(operation) {
+    // @TODO: Apply one string modification
+    const operrationArray = operation.split(" ");
+    const messageData = {
+      type: operrationArray[0],
     };
+    handleMessage(messageData);
+
+    // @TODO: send update to all other clients
   }
 
-  // Create the server
-  const server = new WebSocket.Server({ port: portNumber });
+  // The loop
+  for (let i = 0; i < stringOperations.length; i++) {
+    // Add a task to the event loop
+    doTask(stringOperations[i]);
 
-  server.on("connection", function (serverSocket) {
-    serverSocket.on("message", function (message) {
-      console.log("i am here");
-      serverSocket.send("YAAAYYYY!!!");
-    });
-  });
+    // Wait some time
+    await sleep(1000);
+  }
 
-  // server.listen(portNumber);
-
-  server.on("connection", (socket) => {
-    console.log("connected!");
-  });
-
-  console.log("listening on port " + portNumber);
   // Connect to clients:
   // 1. Connect to clients with greater id
   // 2. Start HTTP server
@@ -72,4 +145,7 @@ function client(id, portNumber, str, clientsList, stringOperations) {
   // 2. Wait some time
 }
 
-exports.startClient = client;
+exports.client = client;
+// exports.startClient = startClient;
+// exports.connectToOtherClients = connectToOtherClients;
+// exports.createServer = createServer;
--- Main.js
+++ Main.js
@@ -1,45 +1,76 @@
 const client = require("./Client");
 
-const inputFilesPaths = [
-  "./Input/input-file-3.txt",
-  "./Input/input-file-2.txt",
-  "./Input/input-file-1.txt",
-];
+async function initilize() {
+  const inputFilesPaths = [
+    "./Input/input-file-1.txt",
+    "./Input/input-file-2.txt",
+    "./Input/input-file-3.txt",
+  ];
 
-var fs = require("fs");
+  var fs = require("fs");
 
-function readInputFile(inputPath) {
-  const data = fs.readFileSync(inputPath, "utf-8", function (err, data) {
+  function readInputFile(inputPath) {
+    const data = fs.readFileSync(inputPath, "utf-8", function (err, data) {
+      return data;
+    });
+
     return data;
-  });
-
-  return data;
-}
-
-function getList(dataArray) {
-  const list = [];
-
-  let str;
-  while (str !== "") {
-    str = dataArray.shift();
-    if (str !== "") {
-      list.push(str);
-    }
   }
-  return list;
+
+  function getList(dataArray) {
+    const list = [];
+
+    let str;
+    while (str !== "") {
+      str = dataArray.shift();
+      if (str !== "") {
+        list.push(str);
+      }
+    }
+    return list;
+  }
+
+  const clientDataArray = [];
+
+  for (let i = 0; i < inputFilesPaths.length; i++) {
+    const data = readInputFile(inputFilesPaths[i]);
+    const dataArray = data.replace(/\r/g, "").split("\n");
+
+    const clientData = {
+      clientId: -1,
+      portNumber: -1,
+      str: "",
+      clientsList: [],
+      stringOperations: [],
+    };
+
+    clientData.clientId = dataArray.shift();
+    clientData.portNumber = dataArray.shift();
+    clientData.str = dataArray.shift();
+    dataArray.shift();
+
+    clientData.clientsList = getList(dataArray);
+    clientData.stringOperations = getList(dataArray);
+
+    clientDataArray.push(clientData);
+    // client.startClient(clientId, portNumber, str, clientsList, stringOperations);
+  }
+
+  for (let i = 0; i < clientDataArray.length; i++) {
+    console.log("create server");
+    await client.client.createServer(
+      clientDataArray[i].clientId,
+      clientDataArray[i].portNumber
+    );
+  }
+
+  for (let i = 0; i < clientDataArray.length; i++) {
+    console.log("connect to other clients");
+    await client.client.connectToOtherClients(
+      clientDataArray[i].clientId,
+      clientDataArray[i].clientsList
+    );
+  }
 }
 
-for (let i = 0; i < inputFilesPaths.length; i++) {
-  const data = readInputFile(inputFilesPaths[i]);
-  const dataArray = data.replace(/\r/g, "").split("\n");
-
-  const clientId = dataArray.shift();
-  const portNumber = dataArray.shift();
-  const str = dataArray.shift();
-  dataArray.shift();
-
-  const clientsList = getList(dataArray);
-  const stringOperations = getList(dataArray);
-
-  client.startClient(clientId, portNumber, str, clientsList, stringOperations);
-}
+initilize();
